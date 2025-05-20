e0a589f9facd63d7abe0bf67dcf2c657f11865dd
Peterson <poseidoncenghizerzhemetmhossad@outlook.com>
2025-05-20 12:04:43 -0300

    Update TodoList.tsx

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,11 +1,9 @@
-// TodoList.tsx
-
 import { Todo } from '../../types/Todo';
 import { TodoItem } from '../TodoItem/TodoItem';
 
 type Props = {
   todos: Todo[];
-  todosDeleting: Todo['id'][]; // IDs das tarefas sendo deletadas
+  todosDeleting: Todo['id'][];
   handleDeleteTodo: (todoId: Todo['id']) => void;
 };
 
@@ -16,18 +14,14 @@ export const TodoList: React.FC<Props> = ({
 }) => {
   return (
     <section className="todoapp__main" data-cy="TodoList">
-      {todos.length > 0 ? (
-        todos.map(todo => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            isLoading={todosDeleting.includes(todo.id)}
-            handleDelete={handleDeleteTodo}
-          />
-        ))
-      ) : (
-        <p className="todoapp__empty">Nenhuma tarefa encontrada.</p>
-      )}
+      {todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          isLoading={todosDeleting.includes(todo.id)}
+          handleDelete={handleDeleteTodo}
+        />
+      ))}
     </section>
   );
 };
